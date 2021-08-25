f2e7ebd42e0dad3f27320c8205c7eafa739a05bd
khei4 <kk.asano.luxy@gmail.com>
2021-08-25 17:22:38 +0900

    check const_propable

--- src/preproc/const_prop.rs
+++ src/preproc/const_prop.rs
@@ -25,6 +25,13 @@ impl FlowGraph {
     }
 }
 
+#[derive(Hash, Debug, Clone, Copy)]
+enum Position {
+    Both,
+    Left,
+    Right,
+}
+
 impl RedStrat for ConstProp {
     fn name(&self) -> &'static str {
         "const_prop"
@@ -35,12 +42,70 @@ impl RedStrat for ConstProp {
         }
     }
 
-    fn apply(&mut self, _instance: &mut PreInstance) -> Res<RedInfo> {
+    fn apply(&mut self, instance: &mut PreInstance) -> Res<RedInfo> {
+        // 消去候補の引数
+        'all_preds: for (pred_idx, pred) in instance.preds().index_iter() {
+            let mut clause_classified_by_pred: ClsMap<Option<Position>> = ClsMap::new();
+            println!("predicate {}, {:#?}", pred_idx, pred);
+
+            // pred's argument is erasable or not
+            let mut pred_args_propable: VarMap<bool> = {
+                let mut vm = VarMap::with_capacity(instance[pred_idx].sig.len());
+                for _ in 0..instance[pred_idx].sig.len() {
+                    vm.push(true);
+                }
+                vm
+            };
+
+            //
+
+            for (cls_idx, clause) in instance.clauses().index_iter() {
+                println!("{:#?}", instance.preds_of_clause(cls_idx));
+                let (pred_apps, head_pred_idx_op) = instance.preds_of_clause(cls_idx);
+
+                let right = match head_pred_idx_op {
+                    Some(h_idx) if h_idx == pred_idx => true,
+                    _ => false,
+                };
+                let left = pred_apps.contains_key(&pred_idx);
+                let position = if left && right {
+                    // check propable
+                    let leftargss = &clause.lhs_preds()[&pred_idx];
+                    let rightargs = clause
+                        .rhs()
+                        .expect(&format!("{}-clause rhs is broken", cls_idx))
+                        .1;
+                    // check arguments
+                    for (rightvaridx, rightarg) in rightargs.index_iter() {
+                        for leftargs in leftargss {
+                            pred_args_propable[rightvaridx] &= leftargs[rightvaridx] == *rightarg;
+                        }
+                    }
+                    if !pred_args_propable.iter().fold(false, |acc, &e| acc || e) {
+                        continue 'all_preds;
+                    }
+                    Some(Position::Both)
+                } else if left {
+                    Some(Position::Left)
+                } else if right {
+                    Some(Position::Right)
+                } else {
+                    None
+                };
+                // TODO: see why I can't use insert on predmap
+                clause_classified_by_pred.push(position);
+            }
+            // 消せるのを見る
+            for (var_idx, _b) in pred_args_propable.index_iter().filter(|(_vid, &b)| b) {
+                println!("{:#?}", var_idx);
+            }
+        }
+
         // 述語ごとに消せる引数を見る
         // 雑に書く
         // # 節を分ける(classify_clause)
+        // - 両辺に現れる(implication)
         // - 右辺にのみ現れる(right)
-        // - 両辺にのみ現れる(implication)
         // - 左辺にのみ現れる(left)
         // # 引数が伝播できるかを見る(check_constpropable??)
         // implicationだけみる
